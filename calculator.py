899ee2a1038282b674f7cc72528adaec2f3893eb
Nikhil Singhal <nikhilagarwal6305@gmail.com>
2026-06-05 09:11:44 +0530

    Added subtraction  operation

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    pass
+
+def division(a, b):
+    pass
+
+def power(a, b):
+    pass
+
+def modulus(a, b):
+    pass
+
+def floor_division(a, b):
+    pass
